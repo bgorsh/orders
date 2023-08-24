6000909d9ca976882a7078ad68674a9ddc4285b5
Борис <80316956+bgorsh@users.noreply.github.com>
2023-08-24 11:18:34 +0300

    some changes in send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -18,8 +18,11 @@ if (mail($to, $subject, $body)) {
 
 // Возвращаем результат обратно в JavaScript-ко
 $message = 'Спасибо за заказ, я Вам перезвоню!';
-echo '<h1 style="width: 100%; position: fixed;
+echo '<h1 style="width: 100%; 
+position: fixed;
 top: 50%;
 left: 50%;
-transform: translate(-50%, -50%);font-size: 68px; text-align: center;">' . $message . '</h1>';
+transform: translate(-50%, -50%);
+font-size: 68px; 
+text-align: center;">' . $message . '</h1>';
 ?>
